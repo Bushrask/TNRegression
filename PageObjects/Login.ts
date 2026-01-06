401a86e4c746cd70faa882d139f51852d8aa67bf
bushra <bushra.shaikh@zeuslearning.com>
2026-01-06 17:47:30 +0530

    adding common and candidate side pages

--- PageObjects/Login.ts
+++ PageObjects/Login.ts
@@ -20,3 +20,62 @@ export class LoginPage {
     await this.loginButton.click({force: true});
   }
 }
+
+export class CommonPages{
+  readonly page: Page;
+  readonly discussionForum: Locator;
+  readonly library: Locator;
+  readonly clinical: Locator;
+  readonly videoLibrary: Locator;
+  readonly profile: Locator;
+  
+  constructor(page: Page) {
+    this.page = page;
+    this.discussionForum = page.getByRole('link', { name: 'Discussion Forum' });
+    this.library = page.locator('//*[@id="library"]');
+    this.clinical = page.getByRole('link', { name: 'Clinical' });
+    this.videoLibrary = page.getByRole('link', { name: 'Video Library' });
+    this.profile = page.getByTitle('Profile ');
+}
+
+  async navigateToCommonPages() {
+    await this.discussionForum.click();
+    await this.page.waitForLoadState('load');
+    await this.library.click();
+    await this.page.waitForLoadState('domcontentloaded');
+    await this.clinical.click();
+    await this.page.waitForLoadState('load');
+    await this.videoLibrary.click();
+    await this.page.waitForLoadState('domcontentloaded');
+    await this.profile.click();
+    await this.page.waitForLoadState('load');
+  }
+}
+
+export class CandidatePages{
+  readonly page: Page;
+  readonly learn: Locator;
+  readonly grades: Locator;
+  readonly managePayments: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.learn = page.getByRole('link', { name: 'Learn' });
+    this.grades = page.getByRole('link', { name: 'Grades' });
+    this.managePayments = page.locator('//*[@id="managePayments"]');
+  
+}
+async navigateToCandidatePages() {
+  if(await this.managePayments.isVisible({timeout:5000})){
+    await this.managePayments.click();
+    await this.page.waitForLoadState('load');  
+    }
+    else{
+      console.log("Manage Payments link is not visible for this user");
+    }
+    await this.learn.click();
+    await this.page.waitForLoadState('load'); 
+    await this.grades.click();
+    await this.page.waitForLoadState('domcontentloaded');
+    
+  }}
--- tests/UserLogins.spec.ts
+++ tests/UserLogins.spec.ts
@@ -1,15 +1,20 @@
 import { Page, Expect, test} from "@playwright/test";    
-import { LoginPage } from "../PageObjects/Login";
+import { LoginPage, CommonPages, CandidatePages } from "../PageObjects/Login";
 
 const login = "https://test.teach-now.com"
 const homePageUrl = "https://test.teach-now.com/home"        
 
-test('User Login Tests', async ({page}) => {
+test('CERT user', async ({page}) => {
     const loginPage = new LoginPage(page);  
+    const commonPages = new CommonPages(page);
+    const candidatePages = new CandidatePages(page);
     await page.goto(login);
 
     // Login to user
     await loginPage.login('vatchina2@gmail.com', 'te@ch');    
     await page.waitForURL(homePageUrl);
 
+    // navigate to common pages
+    //await commonPages.navigateToCommonPages();
+    await candidatePages.navigateToCandidatePages();
 })
