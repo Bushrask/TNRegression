0686363cfe6c7589c8a5b10718a5c426fa84156d
bushra <bushra.shaikh@zeuslearning.com>
2026-05-18 20:18:18 +0530

    cucumber report and timeout changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 
-  timeout: 60_1000, // 100 seconds for each test
+  timeout: 100_1000, // 100 seconds for each test
 
 
   use: {
